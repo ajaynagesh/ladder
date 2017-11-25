b6d9f114c563bfd281d4786d633628999f90cb18
Ajay Nagesh <ajaynagesh@Ajays-MacBook-Pro.local>
2017-11-24 23:50:42 -0700

    One layer CNN on SST2 -- params

--- LeNet_oneLayer.py
+++ LeNet_oneLayer.py
@@ -232,23 +232,23 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     parser = ArgumentParser("An example of training a convolutional network "
                             "on the MNIST dataset.")
-    parser.add_argument("--num-epochs", type=int, default=2,
+    parser.add_argument("--num-epochs", type=int, default=100,
                         help="Number of training epochs to do.")
     parser.add_argument("save_to", default="sst2.pkl", nargs="?",
                         help="Destination to save the state of the training "
                              "process.")
     parser.add_argument("--feature-maps", type=int, nargs='+',
-                        default=[20, 50], help="List of feature maps numbers.")
-    parser.add_argument("--mlp-hiddens", type=int, nargs='+', default=[500],
+                        default=[100], help="List of feature maps numbers.")
+    parser.add_argument("--mlp-hiddens", type=int, nargs='+', default=[100],
                         help="List of numbers of hidden units for the MLP.")
-    parser.add_argument("--conv-sizes", type=int, nargs='+', default=[5, 5],
+    parser.add_argument("--conv-sizes", type=int, nargs='+', default=[(3, 300)],
                         help="Convolutional kernels sizes. The kernels are "
                         "always square.")
-    parser.add_argument("--pool-sizes", type=int, nargs='+', default=[2, 2],
+    parser.add_argument("--pool-sizes", type=int, nargs='+', default=[(61+3-1, 1)],
                         help="Pooling sizes. The pooling windows are always "
                              "square. Should be the same length as "
                              "--conv-sizes.")
-    parser.add_argument("--batch-size", type=int, default=500,
+    parser.add_argument("--batch-size", type=int, default=100,
                         help="Batch size.")
     args = parser.parse_args()
     main(**vars(args))
