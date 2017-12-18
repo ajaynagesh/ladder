a5e71611d2ab645d120a30130d898ddd6bc30549
Ajay Nagesh <ajaynagesh@Ajays-MacBook-Pro.local>
2017-12-18 10:58:20 -0700

    Creating a dataset to model the Emboot setup closely

--- fuel_converter_new.py
+++ fuel_converter_new.py
@@ -127,7 +127,57 @@ def emboot_converter():
     f.flush()
     f.close()
 
+def emboot_converter_traintrain(emboot_dataset):
+    train_vector_features, train_targets, test_vector_features, test_targets = load_emboot_np()
+    f = h5py.File(emboot_dataset, mode='w')
+
+    train_sz = train_vector_features.shape[0]
+    test_sz = test_vector_features.shape[0]
+    feat_sz = train_vector_features.shape[1]
+    dataset_sz = (train_sz + test_sz - 16) * 2 ## NOTE: 13900 * 2 (copy over the train data to the test dataset)
+
+    vector_features = f.create_dataset('features', (dataset_sz, feat_sz), dtype='float64')  ## train + test
+    targets = f.create_dataset('targets', (dataset_sz, 1), dtype='uint8')
+
+    ## put the data loaded into these objects
+
+    train_vector_features_aug = np.vstack([train_vector_features, test_vector_features])[:13900]
+    train_targets_aug = np.vstack([train_targets, test_targets])[:13900]
+
+    vector_features[...] = np.vstack([train_vector_features_aug, train_vector_features_aug])
+    targets[...] = np.vstack([train_targets_aug, train_targets_aug])
+
+    ## label the dims with names
+    vector_features.dims[0].label = 'batch'
+    vector_features.dims[1].label = 'feature'
+    targets.dims[0].label = 'batch'
+    targets.dims[1].label = 'index'
+
+    ## split attribute -- way to recover the splits
+    # creating the split using an API
+    split_dict = {
+        'train': {'features': (0, dataset_sz), 'targets': (0, dataset_sz)},
+         'test': {'features': (0, dataset_sz), 'targets': (0, dataset_sz)}}
+    f.attrs['split'] = H5PYDataset.create_split_array(split_dict)
+
+    f.flush()
+    f.close()
+
+
 class EMBOOT_CONLL(H5PYDataset):
+    u"""EMBOOT dataset
+        transformed to Fuel format
+    """
+    filename = 'conll.traintrain.hdf5'
+
+    def __init__(self, which_sets, **kwargs):
+        kwargs.setdefault('load_in_memory', True)
+        super(EMBOOT_CONLL, self).__init__(
+            file_or_path=find_in_data_path(self.filename),
+            which_sets=which_sets, **kwargs)
+
+
+class EMBOOT_CONLL_SPLIT(H5PYDataset):
     u"""EMBOOT dataset
         transformed to Fuel format
     """
@@ -135,7 +185,7 @@ class EMBOOT_CONLL(H5PYDataset):
 
     def __init__(self, which_sets, **kwargs):
         kwargs.setdefault('load_in_memory', True)
-        super(EMBOOT_CONLL, self).__init__(
+        super(EMBOOT_CONLL_SPLIT, self).__init__(
             file_or_path=find_in_data_path(self.filename),
             which_sets=which_sets, **kwargs)
 
@@ -145,8 +195,11 @@ if __name__ == "__main__":
     # simple_converter()
     #
     # train_set = H5PYDataset('data/simple_dataset.hdf5', which_sets=('train',))
-    emboot_converter()
-    emboot_dataset = "./data/emboot_dataset.new.hdf5"
+    # emboot_converter()
 
-    train_set = H5PYDataset(emboot_dataset, which_sets=('train',))
+    emboot_dataset = "./data/emboot_dataset.traintrain.hdf5"
+    emboot_converter_traintrain(emboot_dataset)
+
+    #
+    # train_set = H5PYDataset(emboot_dataset, which_sets=('train',))
 
--- fuel_converter_onto.py
+++ fuel_converter_onto.py
@@ -47,7 +47,56 @@ def emboot_converter():
     f.flush()
     f.close()
 
+def emboot_converter_traintrain():
+    train_vector_features, train_targets, test_vector_features, test_targets = load_emboot_np()
+    f = h5py.File(emboot_dataset, mode='w')
+
+    train_sz = train_vector_features.shape[0]
+    test_sz = test_vector_features.shape[0]
+    feat_sz = train_vector_features.shape[1]
+    dataset_sz = (train_sz + test_sz - 29) * 2 ## NOTE: 67200 * 2 (copy over the train data to the test dataset)
+
+    vector_features = f.create_dataset('features', (dataset_sz, feat_sz), dtype='float64')  ## train + test
+    targets = f.create_dataset('targets', (dataset_sz, 1), dtype='uint8')
+
+    train_vector_features_aug = np.vstack([train_vector_features, test_vector_features])[:67200]
+    train_targets_aug = np.vstack([train_targets, test_targets])[:67200]
+
+    ## put the data loaded into these objects
+    vector_features[...] = np.vstack([train_vector_features_aug, train_vector_features_aug])
+    targets[...] = np.vstack([train_targets_aug, train_targets_aug])
+
+    ## label the dims with names
+    vector_features.dims[0].label = 'batch'
+    vector_features.dims[1].label = 'feature'
+    targets.dims[0].label = 'batch'
+    targets.dims[1].label = 'index'
+
+    ## split attribute -- way to recover the splits
+    # creating the split using an API
+    split_dict = {
+        'train': {'features': (0, dataset_sz), 'targets': (0, dataset_sz)},
+         'test': {'features': (0, dataset_sz), 'targets': (0, dataset_sz)}}
+    f.attrs['split'] = H5PYDataset.create_split_array(split_dict)
+
+    f.flush()
+    f.close()
+
+
 class EMBOOT_ONTO(H5PYDataset):
+    u"""EMBOOT dataset
+        transformed to Fuel format
+    """
+    filename = 'emboot_dataset.onto.traintrain.hdf5'
+
+    def __init__(self, which_sets, **kwargs):
+        kwargs.setdefault('load_in_memory', True)
+        super(EMBOOT_ONTO, self).__init__(
+            file_or_path=find_in_data_path(self.filename),
+            which_sets=which_sets, **kwargs)
+
+
+class EMBOOT_ONTO_SPLIT(H5PYDataset):
     u"""EMBOOT dataset
         transformed to Fuel format
     """
@@ -55,7 +104,7 @@ class EMBOOT_ONTO(H5PYDataset):
 
     def __init__(self, which_sets, **kwargs):
         kwargs.setdefault('load_in_memory', True)
-        super(EMBOOT_ONTO, self).__init__(
+        super(EMBOOT_ONTO_SPLIT, self).__init__(
             file_or_path=find_in_data_path(self.filename),
             which_sets=which_sets, **kwargs)
 
@@ -65,8 +114,9 @@ if __name__ == "__main__":
     # simple_converter()
     #
     # train_set = H5PYDataset('data/simple_dataset.hdf5', which_sets=('train',))
-    emboot_converter()
-    emboot_dataset = "./data/emboot_dataset.onto.hdf5"
+    # emboot_converter()
+    emboot_dataset = "./data/emboot_dataset.onto.traintrain.hdf5"
+    emboot_converter_traintrain(emboot_dataset)
 
-    train_set = H5PYDataset(emboot_dataset, which_sets=('train',))
+    # train_set = H5PYDataset(emboot_dataset, which_sets=('train',))
 
