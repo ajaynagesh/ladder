6e451baa25343d88cd65bca146c0ac7888e7662d
Ajay Nagesh <ajaynagesh@Ajays-MacBook-Pro.local>
2017-12-28 20:47:32 -0700

    new balanaced fixed seed data

--- run_emboot_fixseeds.py
+++ run_emboot_fixseeds.py
@@ -162,7 +162,7 @@ def make_datastream(dataset, indices, batch_size,
 
     else:
         logger.info("Using fixed set of labeled data points .. from file :=> \"data/seed_ids.training.npy\"")
-        i_labeled = [idx for idx in numpy.load("data/seed_ids.training.npy")]
+        i_labeled = [idx for idx in numpy.load("data/seed_ids.training_new.npy")]
         logger.info('size of i_labeled = %d' % len(i_labeled))
 
     # Get unlabeled indices
